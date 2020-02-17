6e89d77ec2180cd52b5cbe16eda54f67624647d2
manish993 <60160621+manish993@users.noreply.github.com>
2020-02-17 16:19:37 -0500

    Update main.swift
    [mp] supplying dates

--- C0771915_W2020_MID_TERM_MAD3004/C0771915_W2020_MID_TERM_MAD3004/main.swift
+++ C0771915_W2020_MID_TERM_MAD3004/C0771915_W2020_MID_TERM_MAD3004/main.swift
@@ -8,24 +8,45 @@
 
 import Foundation
 
+let calendar = Calendar.current
+let d1 = DateComponents(calendar: calendar, year: 2019, month: 10, day: 26)
+let dm1 = calendar.date(from: i1)
+let calendar = Calendar.current
+let d2 = DateComponents(calendar: calendar, year: 2019, month: 9, day: 25)
+let dm2 = calendar.date(from: i1)
+let calendar = Calendar.current
+let d3 = DateComponents(calendar: calendar, year: 2019, month: 8, day: 24)
+let dh3 = calendar.date(from: i1)
+let calendar = Calendar.current
+let d4 = DateComponents(calendar: calendar, year: 2019, month: 7, day: 23)
+let di4 = calendar.date(from: i1)
+let calendar = Calendar.current
+let d5 = DateComponents(calendar: calendar, year: 2019, month: 6, day: 22)
+let di5 = calendar.date(from: i1)
+let calendar = Calendar.current
+let d6 = DateComponents(calendar: calendar, year: 2019, month: 5, day: 21)
+let di6 = calendar.date(from: i1)
+let calendar = Calendar.current
+let d = DateComponents(calendar: calendar, year: 2019, month: 4, day: 20)
+let d1 = calendar.date(from: i1)
 
 var c1=Customer(customerId: "c1", firstname: "Manish", lastName: "Pawar", email: "manish@gmail.com")
 
-var i1=Internet(providerName:"BSNL", gbUsed: 12, billId:"I01", billDate:Date(), billType: BillType.Internet)
-var m1=Mobile(billId:"M01", billDate:Date(), billType: .Hydro, manufacturerName:"Apple", plan: .Annual, mobileNumber: ",6755577756", gbUsed: 45, minutesUsed: 12)
-var h1=Hydro(agencyName:"Steeles Inc", unitConsumed: 67, billId:"H01", billDate: Date(), billType: .Hydro)
+var i1=Internet(providerName:"BSNL", gbUsed: 12, billId:"I01", billDate:di5, billType: BillType.Internet)
+var m1=Mobile(billId:"M01", billDate:dm1, billType: .Hydro, manufacturerName:"Apple", plan: .Annual, mobileNumber: ",6755577756", gbUsed: 45, minutesUsed: 12)
+var h1=Hydro(agencyName:"Steeles Inc", unitConsumed: 67, billId:"H01", billDate:dh1, billType: .Hydro)
 c1.addBill(bill: m1,Id:m1.billId)
 c1.addBill(bill: h1,Id:h1.billId)
 c1.addBill(bill: i1,Id:i1.billId)
 c1.display()
 var c2=Customer(customerId: "c2", firstname: "Raj", lastName: "kumar", email: "rajkumar@gmail.com")
-var i2=Internet(providerName: "Rogers", gbUsed: 12, billId: "I02", billDate: Date(), billType: .Internet)
-var m2=Mobile(billId: "M02", billDate: Date(), billType: .Mobile, manufacturerName: "Samsung", plan: .monthly, mobileNumber: "1234598765", gbUsed: 12, minutesUsed: 13)
+var i2=Internet(providerName: "Rogers", gbUsed: 12, billId: "I02", billDate:di6, billType: .Internet)
+var m2=Mobile(billId: "M02", billDate:dm2, billType: .Mobile, manufacturerName: "Samsung", plan: .monthly, mobileNumber: "1234598765", gbUsed: 12, minutesUsed: 13)
 c2.addBill(bill: m2, Id: m2.billId)
 c2.addBill(bill: i2, Id: i2.billId)
 c2.display()
 var c3=Customer(customerId: "c3", firstname: "mohit", lastName: "kumar", email: "mohitkumar@yahoo.com")
-var i=Internet(providerName: "Freedom", gbUsed: 12, billId: "I03", billDate: Date(), billType: .Internet)
+var i=Internet(providerName: "Freedom", gbUsed: 12, billId: "I03", billDate:di4, billType: .Internet)
 c1.display()
 var customers = Array<Customer>()
     customers.append(c1)
