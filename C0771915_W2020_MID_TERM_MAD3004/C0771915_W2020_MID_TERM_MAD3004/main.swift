0b8cd72c57c44b7eb3ce3704790bd88fe41c03d5
manish <manishpawar94638@gmail.com>
2020-02-18 17:49:29 -0500

    [mp] setting mobile number

--- C0771915_W2020_MID_TERM_MAD3004/C0771915_W2020_MID_TERM_MAD3004/main.swift
+++ C0771915_W2020_MID_TERM_MAD3004/C0771915_W2020_MID_TERM_MAD3004/main.swift
@@ -33,13 +33,13 @@ let di6 = calendar.date(from: d6)
 var c1=Customer(customerId: "c1", firstname: "Manish", lastName: "Pawar", email: "manish@gmail.com")
 
 var i1=Internet(providerName:"BSNL", gbUsed: 12, billId:"I01", billDate: di5! , billType: BillType.Internet)
-var m1=Mobile(billId:"M01", billDate: dm1!, billType: .Hydro, manufacturerName:"Apple", plan: .Annual, mobileNumber: ",6755577756", gbUsed: 45, minutesUsed: 12)
+var m1=Mobile(billId:"M01", billDate: dm1!, billType: .Hydro, manufacturerName:"Apple", plan: .Annual, mobileNumber: "2355577756", gbUsed: 45, minutesUsed: 12)
 var h1=Hydro(agencyName:"Steeles Inc", unitConsumed: 67, billId:"H01", billDate: dh3!, billType: .Hydro)
 
 
 var c2=Customer(customerId: "c2", firstname: "Raj", lastName: "kumar", email: "rajkumar@gmail.com")
 var i2=Internet(providerName: "Rogers", gbUsed: 12, billId: "I02", billDate:di6!, billType: .Internet)
-var m2=Mobile(billId: "M02", billDate:dm2!, billType: .Mobile, manufacturerName: "Samsung", plan: .monthly, mobileNumber: "+876543212", gbUsed: 12, minutesUsed: 13)
+var m2=Mobile(billId: "M02", billDate:dm2!, billType: .Mobile, manufacturerName: "Samsung", plan: .monthly, mobileNumber: "5876543212", gbUsed: 12, minutesUsed: 13)
 
 
 var c3=Customer(customerId: "c3", firstname: "mohit", lastName: "kumar", email: "mohitkumar@yahoo.com")
