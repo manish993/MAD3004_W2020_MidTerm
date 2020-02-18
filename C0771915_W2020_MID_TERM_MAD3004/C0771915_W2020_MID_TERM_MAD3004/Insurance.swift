2f59a1e043a8c86c48b6d58fb9c29af128f16c61
manish <manishpawar94638@gmail.com>
2020-02-18 18:36:32 -0500

    [mp] insurance class

--- C0771915_W2020_MID_TERM_MAD3004/C0771915_W2020_MID_TERM_MAD3004/Insurance.swift
+++ C0771915_W2020_MID_TERM_MAD3004/C0771915_W2020_MID_TERM_MAD3004/Insurance.swift
@@ -7,16 +7,38 @@
 //
 
 import Foundation
-//enum InsuranceType{
-//    case mobile, home
-//}
-//class Insurance: Bill
-//{
- //   var insuranceProvider:String
-   // var insuranceType:InsuranceType
-   // var insuranceStartDate:Date
-    //var insuranceEndDate: Date
-    ///var totalDays: Int
-   /// var totalInstallment: Double
+enum InsuranceType{
+ case mobile, home
+}
+class Insurance: Bill
+{
+    var insuranceProvider:String
+    var insuranceType:InsuranceType
+    var insuranceStartDate:Date
+    var insuranceEndDate: Date
+    var totalDays: Int=0
+    var totalInstallment: Int
     
-    //init(insuranceProvider:String,insuranceType:InsuranceType,insuranceStartDate:Date,insuranceEndDate: Date,
+    init(insuranceProvider:String,insuranceType:InsuranceType,insuranceStartDate:Date,insuranceEndDate: Date,totalInstallment:Int,billId:String,billDate:Date,billType:BillType)
+    {
+        self.insuranceProvider=insuranceProvider
+        self.insuranceType=insuranceType
+        self.insuranceStartDate=insuranceStartDate
+        self.insuranceEndDate=insuranceEndDate
+        self.totalInstallment=totalInstallment
+        super.init(billId:billId,billDate:billDate,billType:billType)
+        
+    }
+    
+    
+    override func display() {
+        super.display()
+        print("\t Provider Name : \(self.insuranceProvider)")
+        print("\t Provider Type : \(self.insuranceType)")
+        let date = insuranceStartDate.formatDate(date: insuranceStartDate)
+        print("\t Start Date : \(date)")
+        let date2 = insuranceEndDate.formatDate(date: insuranceEndDate)
+        print("\t End Date : \(date2)")
+        print("\t Total Day : \(self.totalDays)")
+        print("\t Total Installments : \(self.totalInstallment)")
+    }}
