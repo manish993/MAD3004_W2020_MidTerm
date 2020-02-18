8e02323ba60aec25731ed92be955524b95df6fbf
manish <manishpawar94638@gmail.com>
2020-02-18 13:13:56 -0500

    [mp] date extension

---
+++ C0771915_W2020_MID_TERM_MAD3004/C0771915_W2020_MID_TERM_MAD3004/DateExtension.swift
@@ -0,0 +1,16 @@
+//
+//  DateExtension.swift
+//  C0771915_W2020_MID_TERM_MAD3004
+//
+//  Created by MacStudent on 2020-02-18.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import Foundation
+extension Date {
+func formatDate(date : Date) -> String {
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "EEEE, dd MMMM, yyyy"
+    return dateFormatter.string(from: date)
+}
+}
